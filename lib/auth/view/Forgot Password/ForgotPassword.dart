35e546434252f62deb43c9811c42bb5df3f478f8
Abdelrhman Mohamed Ezz-Eldin <62518324+tron200@users.noreply.github.com>
2023-05-05 22:05:12 +0300

    Sign up

---
+++ lib/auth/view/Forgot Password/ForgotPassword.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class ForgotPassword extends StatefulWidget {
+
+  @override
+  State<ForgotPassword> createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
