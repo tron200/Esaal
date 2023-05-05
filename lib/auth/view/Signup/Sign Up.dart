e7fd4f97b0344527f3749748e08ae6f7106ff5aa
OmarElmorsy <119528435+OmarElmorsy@users.noreply.github.com>
2023-05-05 18:11:14 +0300

    Signup

--- lib/auth/view/Signup/Sign Up.dart
+++ lib/auth/view/Signup/Sign Up.dart
@@ -14,144 +14,130 @@ class _SignUp extends State<SignUp> {
     return Scaffold(
       backgroundColor: const Color(0xff124559),
       body: Container(
-        width: 440,
-        height: 800,
+
         margin: EdgeInsets.fromLTRB(0, 100, 0, 0),
         child: Stack(
           children: [
             const Positioned(
-              width: 336,
-              height: 241,
               left: 50,
-              top: 140,
-              child: Image(image: AssetImage("images/SignUP.png")),
+              child: Image(image: AssetImage("assets/images/SignUP.png")),
             ),
-            Center(
-              child: Container(
-                width: 348,
-                height: 260,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  gradient: const LinearGradient(colors: [
-                    Color.fromRGBO(217, 217, 217, 0.8),
-                    Color.fromRGBO(217, 217, 217, 0.6),
-                  ]),
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      height: 50,
-                      padding:const EdgeInsets.fromLTRB(30, 0, 0, 0),
-                      child:  Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            "Create a new account",
-                            textAlign:TextAlign.end,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15,
-                            ),
+            Container(
+              margin: EdgeInsets.only(top: 130, left: 30, right: 30, bottom: 300),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                gradient: const LinearGradient(colors: [
+                  Color.fromRGBO(217, 217, 217, 0.8),
+                  Color.fromRGBO(217, 217, 217, 0.6),
+                ]),
+              ),
+              child: Column(
+
+                children: [
+                  SizedBox(height: 20,),
+                  Container(
+                    height: 50,
+                    padding:const EdgeInsets.only(left: 30),
+                    child:  Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Create a new account",
+                          textAlign:TextAlign.end,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
                           ),
-                          Row(
-                            children:  [
-                              const Text(
-                                "Already have one?",
+                        ),
+                        Row(
+                          children:  [
+                            const Text(
+                              "Already have one?",
+                              textAlign:TextAlign.end,
+                              style: TextStyle(
+                                color:Colors.white ,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 13,
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: () {},
+                              child: const Text(
+                                " Log In",
                                 textAlign:TextAlign.end,
                                 style: TextStyle(
-                                  color:Colors.white ,
+                                  color: Color(0xffFFA500),
                                   fontWeight: FontWeight.bold,
                                   fontSize: 13,
                                 ),
                               ),
-                              InkWell(
-                                onTap: () {},
-                                child: const Text(
-                                  " Log In",
-                                  textAlign:TextAlign.end,
-                                  style: TextStyle(
-                                    color: Color(0xffFFA500),
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 13,
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      width: 288,
-                      height: 57,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: Colors.blue,
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        //  start (or) create
-                        Container(
-                          width: 160,
-                          decoration: const BoxDecoration(
-                            border: Border(
-                              bottom:
-                              BorderSide(color: Colors.black, width: 1.0),
-                            ),
-                          ),
+                            )
+                          ],
                         ),
-                        const SizedBox(
-                          width: 5,
-                        ),
-                        const Text(
-                          "or",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15),
-                        ),
-                        const SizedBox(
-                          width: 5,
-                        ),
-                        Container(
-                          width: 160,
-                          decoration: const BoxDecoration(
-                            border: Border(
-                              bottom:
-                              BorderSide(color: Colors.black, width: 1.0),
-                            ),
-                          ),
-                        ),
-                        //  end (or) create
                       ],
                     ),
-                    ElevatedButton(
-                        style: TextButton.styleFrom(
-                          backgroundColor: const Color(0xff124559),
-                          fixedSize: const Size(281, 45),
-                          shape: const RoundedRectangleBorder(
-                              borderRadius:
-                              BorderRadius.all(Radius.circular(12))),
-                        ),
-                        onPressed: () {},
-                        child: const Text(
-                          "sign up with email",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 19,
-                              fontWeight: FontWeight.bold),
-                        )),
-                  ],
-                ),
+                  ),
+                  SizedBox(height: 20,),
+                  Container(
+                    width: 288,
+                    height: 57,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.blue,
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  Row(
+                    children:const [
+                      //  start (or) create
+                      Expanded(
+                        child:Divider(color: Colors.black, thickness: 1.5,),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        "or",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Expanded(
+                        child:Divider(color: Colors.black, thickness: 1.5,),
+                      ),
+                      //  end (or) create
+                    ],
+                  ),
+                  SizedBox(height: 20,),
+                  ElevatedButton(
+                      style: TextButton.styleFrom(
+                        backgroundColor: const Color(0xff124559),
+                        fixedSize: Size(230,40),
+                        shape: const RoundedRectangleBorder(
+                            borderRadius:
+                            BorderRadius.all(Radius.circular(12))),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        "sign up with email",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 19,
+                            fontWeight: FontWeight.bold),
+                      )),
+                  SizedBox(height: 20,),
+                ],
               ),
             ),
             const Positioned(
                 left: 50,
-                top: 190,
+                top: 50,
                 child: Text(
                   "Sign up",
                   style: TextStyle(
